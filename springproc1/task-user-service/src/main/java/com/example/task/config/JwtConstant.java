bc749b766dd898887d7d8cf123263154efbff4e9
surya21102003 <156424142+surya21102003@users.noreply.github.com>
2025-07-26 16:30:26 +0530

    Delete springproc1/task-user-service directory

--- springproc1/task-user-service/src/main/java/com/example/task/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.example.task.config;
-
-public class JwtConstant {
-    public static final String SECRET_KEY="adsfdsfsdfsdssd";
-    public static final String JWT_HEADER="Authorization";
-
-}
--- springproc1/task-user-service/src/main/java/com/example/task/repositories/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.task.repositories;
-
-import com.example.task.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Long> {
-    User findByEmail(String email);
-}
