5914ddf3dc7b733508b71751a3d560ddcc17acac
surya21102003 <156424142+surya21102003@users.noreply.github.com>
2025-09-01 21:13:41 +0530

    Add files via upload

---
+++ taskmanagement(microservices)/backend/task-notification-service/src/main/java/com/zosh/tasknotificationservice/TaskNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zosh.tasknotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ taskmanagement(microservices)/backend/task-notification-service/src/test/java/com/zosh/tasknotificationservice/TaskNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zosh.tasknotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
